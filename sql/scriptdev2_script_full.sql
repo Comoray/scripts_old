72c78032fe53ac926305acc69b51c01cd63e17c7
Schmoozerd <schmoozerd@scriptdev2.com>
2013-04-13 01:46:30 +0300

    [z2668] Onyxia Improvements. Requires CMaNGOS [z2333]
    Also add some candy to Onyxia script suggested and prepared by Wilmaa
    
    (based on commit e217657)
    
    Signed-off-by: Xfurry <xfurry@scriptdev2.com>
    
    (based on commit SD2-TBC[s2715] - 1d911d3)
    
    Signed-off-by: Xfurry <xfurry@scriptdev2.com>

--- sql/scriptdev2_script_full.sql
+++ sql/scriptdev2_script_full.sql
@@ -3,7 +3,7 @@
 --
 
 DELETE FROM sd2_db_version;
-INSERT INTO sd2_db_version (version) VALUES ('ScriptDev2 (for MaNGOSZero z2374+) ');
+INSERT INTO sd2_db_version (version) VALUES ('ScriptDev2 (for MaNGOSZero z2377+) ');
 
 --
 -- Below contains data for table `script_texts` mainly used in C++ parts.
